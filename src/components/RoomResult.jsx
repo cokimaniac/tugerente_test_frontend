9dd22d40fb5b5ebc21e8e3b12ddf96e936e70704
Jorge Luis <cokimaniac@gmail.com>
2021-11-19 17:54:58 -0400

    RoomResult component information to show modified

--- src/components/RoomResult.jsx
+++ src/components/RoomResult.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom"
+
 function RoomResult(props) {
     return (
         <div className="card" style={{width: "18rem"}}>
             <div className="card-body">
-                <h5 className="card-title">Tipo: {props.room.type}</h5>
+                <h5 className="card-title">
+                    Tipo: {props.room.type}
+                    {props.room.availability ? <span className="badge" style={{background: "#fac31d"}}>Disponible</span> : <span className="badge" style={{background: "#ef4f2a"}}>No Disponible</span>}
+                </h5>
                 <p className="card-text">{props.room.details}</p>
-                <a href="/" className="btn btn-primary">Hacer Reserva</a>
+                <p><b>Precio: </b>{ props.room.price } Bs.</p>
+                <Link to={ "/rooms/" + props.room.id } className="btn btn-primary">Hacer Reserva</Link>
             </div>
         </div>
     )
